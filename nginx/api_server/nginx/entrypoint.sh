c69bb9bebd71e63689de70aa7320567659a765ff
Rohit-kaushik45 <rkkau12@gmail.com>
2025-10-07 23:32:02 +0530

    changing http to https and made some changes
    Signed-off-by: Abhishek-Punhani <punhani.manavabhi@gmail.com>

--- nginx/api_server/nginx/entrypoint.sh
+++ nginx/api_server/nginx/entrypoint.sh
@@ -2,8 +2,6 @@
 set -e
 
 # Substitute environment variables in nginx.conf.template to nginx.conf
-if [ -f /etc/nginx/nginx.conf.template ]; then
-  envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-fi
+envsubst '${API_BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-exec nginx -g 'daemon off;'
+echo "Nginx config generated with API_BACKEND_URL=${API_BACKEND_URL}"
--- nginx/auth_server/nginx/entrypoint.sh
+++ nginx/auth_server/nginx/entrypoint.sh
@@ -2,8 +2,6 @@
 set -e
 
 # Substitute environment variables in nginx.conf.template to nginx.conf
-if [ -f /etc/nginx/nginx.conf.template ]; then
-  envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-fi
+envsubst '${AUTH_BACKEND_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
-exec nginx -g 'daemon off;'
+echo "Nginx config generated with AUTH_BACKEND_URL=${AUTH_BACKEND_URL}"
